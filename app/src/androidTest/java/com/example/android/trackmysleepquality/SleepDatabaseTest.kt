f29018667215688f06e02b215f2105213d900e73
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-11-22 12:00:36 +0300

    Added Unit Test for Dao Functions.

--- app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
+++ app/src/androidTest/java/com/example/android/trackmysleepquality/SleepDatabaseTest.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.trackmysleepquality
 
+import android.util.Log
 import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
@@ -42,7 +43,7 @@ class SleepDatabaseTest {
     private lateinit var sleepDao: SleepDatabaseDao
     private lateinit var db: SleepDatabase
 
-    // 1. Adım olarak DataBase oluşturuluyor.
+    // 1. Adım olarak DataBase oluşturuluyor. Database inMemory ile birlikte Bellek üzerinde oluşturuluyor.
     @Before // İlk yapılması gereken işlem
     fun createDb() {
         val context = InstrumentationRegistry.getInstrumentation().targetContext
@@ -61,7 +62,7 @@ class SleepDatabaseTest {
         db.close()
     }
 
-    @Test // 2. Adım Test Yapılır.
+    @Test // 2. Adım Test Yapılır.  Dao Insert Function Checked
     @Throws(Exception::class)
     fun insertAndGetNight() {
         val night = SleepNight() // Dummy Veri
@@ -69,4 +70,75 @@ class SleepDatabaseTest {
         val tonight = sleepDao.getTonight() // En son eklenen dummy veri
         assertEquals(tonight?.sleepQuality, -1) // eşit mi değil mi
     }
+
+
+    /**
+     * Dao Update Function
+     */
+    @Test // Dao Update Function Checked
+    @Throws(java.lang.Exception::class)
+    fun updateAndGetNight() {
+        val night = SleepNight() // Dummy Veri
+        night.sleepQuality = 5
+        night.startTimeMilli = 15
+        night.stopTimeMilli = 20
+        sleepDao.insert(night) // Dummy insert
+        val tonight = sleepDao.getTonight()
+        tonight?.stopTimeMilli = 30
+        tonight?.sleepQuality = 10
+        tonight?.startTimeMilli = 50
+        sleepDao.update(tonight!!)
+        //Log.i("Night Quality",""+tonight.sleepQuality)
+        //Log.i("Night StartTimeMilli",""+tonight.startTimeMilli)
+        //Log.i("Night StopTimeMilli",""+tonight.stopTimeMilli)
+    }
+
+    @Test // Dao Get Function Checked
+    @Throws(java.lang.Exception::class)
+    fun getAndGetNight() {
+
+        val night = SleepNight()
+        night.sleepQuality = 10;
+        night.startTimeMilli = 10;
+        night.stopTimeMilli = 10;
+        night.nightId = 5;
+        sleepDao.insert(night)
+        val getNight = sleepDao.get(5)
+        //Log.i("Night Quality",""+getNight?.sleepQuality)
+        //Log.i("Night StartTimeMilli",""+getNight?.startTimeMilli)
+        //Log.i("Night StopTimeMilli",""+getNight?.stopTimeMilli)
+    }
+
+    @Test // Dao Clear Function Checked
+    @Throws(java.lang.Exception::class)
+    fun clearTableData() {
+
+        for (i in 1..10) {
+            val night = SleepNight()
+            night.nightId = i.toLong()
+            sleepDao.insert(night)
+        }
+
+        for (i in 1..10) {
+
+            Log.i("Night Value = > ", "" + sleepDao.get(i.toLong())?.nightId)
+
+        }
+
+        sleepDao.clear()
+
+        for (i in 1..10) {
+
+            Log.i("Night Value = > ", "" + sleepDao.get(i.toLong())?.nightId)
+
+        }
+
+    }
+
+
+
+
+
+
+
 }
