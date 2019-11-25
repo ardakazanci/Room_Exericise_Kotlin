ee5036e591ca62814fd53acc02a878d75a79ebee
Arda Kazancı <ardakazanci@anadolu.edu.tr>
2019-11-25 12:22:35 +0300

    Sleep Quality point function added.

--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityFragment.kt
@@ -22,7 +22,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.android.trackmysleepquality.R
+import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepQualityBinding
 
 /**
@@ -33,19 +37,39 @@ import com.example.android.trackmysleepquality.databinding.FragmentSleepQualityB
  */
 class SleepQualityFragment : Fragment() {
 
-    /**
-     * Called when the Fragment is ready to display content to the screen.
-     *
-     * This function uses DataBindingUtil to inflate R.layout.fragment_sleep_quality.
-     */
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
-        // Get a reference to the binding object and inflate the fragment views.
+
         val binding: FragmentSleepQualityBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_sleep_quality, container, false)
 
         val application = requireNotNull(this.activity).application
+
+        // Gelen argüman - key değeri arguments aracılığıyla alınacak.
+        val arguments = SleepQualityFragmentArgs.fromBundle(arguments!!)
+
+        // SleepDao nesnesi
+        val dataSource = SleepDatabase.getInstance(application).sleepDatabaseDao
+
+        // ViewModel oluşturan factory objesi.
+        val viewModelFactory = SleepQualityViewModelFactory(arguments.sleepNightKey, dataSource)
+
+        val sleepQualityViewModel =
+                ViewModelProviders.of(
+                        this, viewModelFactory).get(SleepQualityViewModel::class.java)
+
+        // UI Görünümü nesneleriyl viewmodel ' ı birleştiriyoruz.
+        binding.sleepQualityViewModel = sleepQualityViewModel
+
+        sleepQualityViewModel.navigateToSleepTracker.observe(this, Observer {
+            if (it == true) { // Observed state is true.
+                this.findNavController().navigate(
+                        SleepQualityFragmentDirections.actionSleepQualityFragmentToSleepTrackerFragment())
+                sleepQualityViewModel.doNavigateUp()
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleepquality/SleepQualityViewModel.kt
@@ -15,3 +15,53 @@
  */
 
 package com.example.android.trackmysleepquality.sleepquality
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.android.trackmysleepquality.database.SleepDatabase
+import com.example.android.trackmysleepquality.database.SleepDatabaseDao
+import kotlinx.coroutines.*
+
+/**
+ * @param sleepNightKey : Üzerinde puan verilecek SleepNight nesnesi index i
+ * @param database : Veritabanı işlemleri için Dao nesnesi.
+ */
+class SleepQualityViewModel(
+        private val sleepNightKey: Long = 0L,
+        val database: SleepDatabaseDao) : ViewModel() {
+
+    // ViewModel içerisinde kullanılacak coroutine ' nin yönetileceği iş nesnesi.
+    private val viewModelJob = Job()
+    // Coroutine sonuçları viewmodel kapsamı içerisinde çalışacak. Çünkü viewModel sonuçları Main Thread üzerinde işlenecek
+    private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
+
+    private val _navigateToSleepTracker = MutableLiveData<Boolean>()
+    val navigateToSleepTracker: LiveData<Boolean?>
+        get() = _navigateToSleepTracker
+
+    fun doNavigateUp() {
+        _navigateToSleepTracker.value = null
+    }
+
+    // Seçilen puana göre ilgili key ' e ait SleepNight nesnesini güncelle.
+    fun onSetSleepQuality(quality: Int) {
+        uiScope.launch {
+
+            withContext(Dispatchers.IO) {
+                // withContext aslında globalScope.async
+                val tonight = database.get(sleepNightKey) ?: return@withContext
+                tonight.sleepQuality = quality
+                database.update(tonight)
+            }
+            _navigateToSleepTracker.value = true
+        }
+    }
+
+    // ViewModel öldürüğünde coroutine ' ni sonlandır.
+    override fun onCleared() {
+        super.onCleared()
+        viewModelJob.cancel()
+    }
+
+}
